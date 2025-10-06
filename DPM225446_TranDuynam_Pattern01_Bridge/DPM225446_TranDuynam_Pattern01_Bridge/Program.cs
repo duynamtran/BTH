936a16a5ce4af952dd15dc8975b7bc349d9bb16a
duynamtran <duynamtran204@gmail.com>
2025-10-06 09:19:37 +0700

    Add files via upload

---
+++ DPM225446_TranDuynam_Pattern01_Bridge/DPM225446_TranDuynam_Pattern01_Bridge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225446_TranDuynam_Pattern01_Bridge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+            // Set implementation and call
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+            // Change implemention and call
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
