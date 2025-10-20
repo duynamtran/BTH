c2cb9cb88e31240ebb38251ff12163643b1174ce
duynamtran <duynamtran204@gmail.com>
2025-10-20 07:51:43 +0700

    Add files via upload

---
+++ After_ExtractMethod_01/Customers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace After_ExtractMethod_01
+{
+    public class Customers
+    {
+        private string name;
+        private List<double> purchases = new List<double>();
+
+        public Customers(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddPurchase(double amount)
+        {
+            purchases.Add(amount);
+        }
+
+        // Tính tổng số tiền nợ (outstanding)
+        public double GetOutstanding()
+        {
+            double total = 0;
+            foreach (var amount in purchases)
+            {
+                total += amount;
+            }
+            return total;
+        }
+
+        // Hàm in banner
+        private void PrintBanner()
+        {
+            Console.WriteLine("**************************");
+            Console.WriteLine("**** Customer Owes ******");
+            Console.WriteLine("**************************");
+        }
+
+        // Hàm in chi tiết nợ
+        private void PrintDetails()
+        {
+            Console.WriteLine("name: " + this.name);
+            Console.WriteLine("amount: " + this.GetOutstanding());
+        }
+
+        // Hàm chính gọi banner + chi tiết
+        public void PrintOwing()
+        {
+            this.PrintBanner();
+            this.PrintDetails();
+        }
+    }
+}
---
+++ After_ExtractMethod_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace After_ExtractMethod_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Tạo khách hàng
+            Customers customer = new Customers("Alice");
+
+            // Thêm các đơn hàng
+            customer.AddPurchase(120.50);
+            customer.AddPurchase(89.90);
+            customer.AddPurchase(50.00);
+
+            // In thông tin nợ
+            customer.PrintOwing();
+
+            Console.ReadKey();
+        }
+    }
+}
