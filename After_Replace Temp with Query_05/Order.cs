6d8ef75962f4b29c47b9c7b3e304dee4af6a5817
duynamtran <duynamtran204@gmail.com>
2025-10-20 08:13:34 +0700

    Add files via upload

---
+++ After_Replace Temp with Query_05/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace After_Replace_Temp_with_Query_05
+{
+    class Order
+    {
+        private int quantity;
+        private double itemPrice;
+
+        public Order(int quantity, double itemPrice)
+        {
+            this.quantity = quantity;
+            this.itemPrice = itemPrice;
+        }
+
+        private double BasePrice()
+        {
+            return quantity * itemPrice;
+        }
+
+        public double CalculateTotal()
+        {
+            if (BasePrice() > 1000)
+            {
+                return BasePrice() * 0.95;  // giảm 5%
+            }
+            else
+            {
+                return BasePrice() * 0.98;  // giảm 2%
+            }
+        }
+
+        public void PrintInvoice()
+        {
+            Console.WriteLine($"Số lượng: {quantity}");
+            Console.WriteLine($"Giá gốc: {BasePrice()}");
+            Console.WriteLine($"Tổng thanh toán: {CalculateTotal()}");
+        }
+    }
+}
---
+++ After_Replace Temp with Query_05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace After_Replace_Temp_with_Query_05
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== AFTER REFACTORING ===");
+            Order order1 = new Order(5, 150);
+            Order order2 = new Order(20, 60);
+
+            order1.PrintInvoice();
+            Console.WriteLine();
+            order2.PrintInvoice();
+        }
+    }
+}
