84c37ed2bde9b2034019bbd8a1c700d2a663ab3d
duynamtran <duynamtran204@gmail.com>
2025-10-20 09:17:47 +0700

    Add files via upload

---
+++ 10_MoveMethod/After_MoveMethod_10/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Class1
+{
+    private Class2 class2;
+
+    public Class1(Class2 c2)
+    {
+        class2 = c2;
+    }
+
+    // Xóa hoặc chuyển hướng sang Class2
+    public void AMethod()
+    {
+        class2.AMethod();
+    }
+}
+
+public class Class2
+{
+    public void AMethod()
+    {
+        Console.WriteLine("This method now lives in Class2");
+        DoSomething();
+    }
+
+    public void DoSomething()
+    {
+        Console.WriteLine("Doing something...");
+    }
+}
---
+++ 10_MoveMethod/Before_MoveMethod_10/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Class1
+{
+    private Class2 class2;
+
+    public Class1(Class2 c2)
+    {
+        class2 = c2;
+    }
+
+    // Phương thức này được Class2 dùng nhiều hơn
+    public void AMethod()
+    {
+        Console.WriteLine("This method does something related to Class2");
+        class2.DoSomething();
+    }
+}
+
+public class Class2
+{
+    public void UseClass1(Class1 c1)
+    {
+        // Class2 thường xuyên gọi AMethod()
+        c1.AMethod();
+    }
+
+    public void DoSomething()
+    {
+        Console.WriteLine("Doing something...");
+    }
+}
